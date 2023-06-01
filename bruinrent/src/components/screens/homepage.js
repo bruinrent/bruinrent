276003ebaed541b4a662a1ebe280ca5b60e98c05
Ashita Singh <ashisin17@gmail.com>
2023-06-01 00:46:32 -0700

    Home page added apartment and details

--- bruinrent/src/components/screens/homepage.js
+++ bruinrent/src/components/screens/homepage.js
@@ -7,27 +7,37 @@ import logo from '../../assets/logo_white.png';
 
 const Homepage = () => {
   return (
-    <div className="homepage-container">
-      <div className="homepage-boxtop">
-        <div className="homepage-content">
-          <h2 className="homepage-header">BruinRent</h2>
-          <h1 className="homepage-title">Housing Made Easy For Bruins.</h1>
-          <button className="homepage-button1">List With Us</button>
-          <button className="homepage-button2">Sign In</button>
-          <button className="homepage-button3">
-            <span className="button3-text">Look for an Apartment</span>
-          </button>
-          <img className="homepage-logo" src={logo} alt="Bruin Rent Logo" />
-          <button className="homepage-instagram">
-              {/* <img className="homepage-instagram1" src={instagram1} alt="ig1" />
-              <img className="homepage-instagram2" src={instagram2} alt="ig1" /> */}
-          </button>
-          <div className="homepage-boxbot">
-            <h3>Additional Content Here</h3>
-          </div>
-        </div>
+<div className="homepage-container">
+  <div className="homepage-boxtop">
+    <div className="homepage-content">
+      <h2 className="homepage-header">BruinRent</h2>
+      <h1 className="homepage-title">Housing Made Easy For Bruins.</h1>
+      <button className="homepage-button1">List With Us</button>
+      <button className="homepage-button2">Sign In</button>
+      <button className="homepage-button3">
+        <span className="button3-text">Look for an Apartment</span>
+      </button>
+      <img className="homepage-logo" src={logo} alt="Bruin Rent Logo" />
+    </div>
+  </div>
+  <div className="homepage-boxbot">
+    <h3 className="homepage-populartext">Popular Apartments Near You:</h3>
+    <div className="homepage-image-container">
+      <div className="homepage-image-item">
+        <img className="homepage-image" src={apart1} alt="Apartment 1" />
+        <p className="homepage-image-address">123 Main St</p>
+      </div>
+      <div className="homepage-image-item">
+        <img className="homepage-image" src={apart2} alt="Apartment 2" />
+        <p className="homepage-image-address">456 Elm St</p>
+      </div>
+      <div className="homepage-image-item">
+        <img className="homepage-image" src={apart3} alt="Apartment 3" />
+        <p className="homepage-image-address">789 Oak St</p>
       </div>
     </div>
+  </div>
+</div>
   );
 }
 
