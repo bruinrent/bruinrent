c6c7daebd239f8d6a871ee81b211598ef700633b
vikrampuliyadi <vikram.puliyadi@gmail.com>
2023-06-02 15:10:52 -0700

    waitlist updates firebase - low security

--- bruinrent/src/components/screens/waitlist.js
+++ bruinrent/src/components/screens/waitlist.js
@@ -1,25 +1,63 @@
-import React from 'react';
-import logo from '../../assets/BruinRentLogo.png';
-import './homepage.css'; // Import a separate CSS file for component-specific styles
-import instagram1 from '../../assets/Instagram.png';
-import instagram2 from '../../assets/Instagram2.png';
+import React, { useState } from "react";
+import logo from "../../assets/BruinRentLogo.png";
+import { collection, addDoc } from "firebase/firestore";
+import { app, firestore } from "../../firebase";
+import "./homepage.css"; // Import a separate CSS file for component-specific styles
+import instagram1 from "../../assets/Instagram.png";
+import instagram2 from "../../assets/Instagram2.png";
 
 const Waitlist = () => {
-  return (
-    <div className="waitlist-container">
-      <div className="waitlist-content">
-        <h2 className="waitlist-header">BruinRent</h2>
-        <h1 className="waitlist-title">Housing Made Easy For Bruins.</h1>
-        <p className="waitlist-italics">Coming Soon</p>
-        <button className="waitlist-waitlist">Join the Waitlist</button>
-        <img className="waitlist-logo" src={logo} alt="Bruin Rent Logo" />
-        <button className="waitlist-instagram">
-            <img className="waitlist-instagram1" src={instagram1} alt="ig1" />
-            <img className="waitlist-instagram2" src={instagram2} alt="ig1" />
-        </button>
-      </div>
-    </div>
-  );
-}
+    const [email, setEmail] = useState("");
+    const handleClick = async () => {
+        try {
+            // Create a new document in the "waitlist" collection with the email
+            await addDoc(collection(firestore, "waitlist"), { email });
+            console.log("Waitlist joined!");
+        } catch (error) {
+            console.error("Error adding document: ", error);
+        }
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    return (
+        <div className="waitlist-container">
+            <div className="waitlist-content">
+                <h2 className="waitlist-header">BruinRent</h2>
+                <h1 className="waitlist-title">
+                    Housing Made Easy For Bruins.
+                </h1>
+                <p className="waitlist-italics">Coming Soon</p>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+                <button className="waitlist-waitlist" onClick={handleClick}>
+                    Join the Waitlist
+                </button>
+                <img
+                    className="waitlist-logo"
+                    src={logo}
+                    alt="Bruin Rent Logo"
+                />
+                <button className="waitlist-instagram">
+                    <img
+                        className="waitlist-instagram1"
+                        src={instagram1}
+                        alt="ig1"
+                    />
+                    <img
+                        className="waitlist-instagram2"
+                        src={instagram2}
+                        alt="ig1"
+                    />
+                </button>
+            </div>
+        </div>
+    );
+};
 
 export default Waitlist;
--- bruinrent/src/firebase.js
+++ bruinrent/src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+//import { getDatabase, ref, push, set } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export { app, firestore };
