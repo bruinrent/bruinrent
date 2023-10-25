9d60751c628614f568b5f99c54f6ea8b221d4b82
Ashita Singh <ashisin17@gmail.com>
2023-10-25 00:32:06 -0700

    Sign-in page initial set up

--- bruinrent/src/components/Header.jsx
+++ bruinrent/src/components/Header.jsx
@@ -1,8 +1,33 @@
 import "../styles/Header.css";
 import { Link } from "react-router-dom";
 import logo from "../assets/logo_white.png";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const Header = () => {
+  const handleSignInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
   return (
     <div className="header-container">
       <Link to="/" className="header-logo-container">
@@ -14,9 +39,7 @@ const Header = () => {
       </Link>
 
       <div className="header-links">
-        <Link to="/ConstructionPage">
-          <button className="header-button">Sign In</button>
-        </Link>
+          <button className="header-button" onClick={handleSignInWithGoogle}>Sign In</button>
         <Link
           target="_blank"
           to="https://docs.google.com/forms/d/e/1FAIpQLSfHY7fNYDFBbNYr3Xy4caIz7yqNmvDnYfAB9HZlq1aH7vl0Qw/viewform"
