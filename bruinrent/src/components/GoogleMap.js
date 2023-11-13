b431e70d091e5cc62f12e0aa10e3579e1b9cc8ab
vikrampuliyadi <vikram.puliyadi@gmail.com>
2023-11-12 23:22:18 -0800

    map marker updated

--- bruinrent/src/components/GoogleMap.js
+++ bruinrent/src/components/GoogleMap.js
@@ -1,87 +1,92 @@
-import GoogleMap from 'google-maps-react-markers'
+import GoogleMap from "google-maps-react-markers";
 import React from "react";
-import { useState, useRef} from "react";
+import { useState, useRef } from "react";
 import pin from "../assets/Google_Maps_pin.svg";
+import MapMarker from "../assets/MapMarker.png";
 import { Link, useNavigate } from "react-router-dom";
-
 
 const APIKey = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
-const Marker = ({ text, onClick}) => (
-  <div className='pin-container' onClick={onClick}>
-     <img src={pin} alt="Pin" />
-    {/* <div className="pin" style={{zIndex:'9999'}}/> */}
-    <p style={{textAlign:'center', width:'5rem'}}>{text}</p>
-  </div>
-)
-
-const App = ( {markers} ) => {
-  const navigate = useNavigate();
-
-if (!APIKey) {
-  console.error('Did you set the required environment variables in .env?');
-  process.exit(1); // Exit the application with an error code
-}
-  const mapRef = useRef(null)
-  const [mapReady, setMapReady] = useState(false)
-
-
-
-
-  /**
-   * @description This function is called when the map is ready
-   * @param {Object} map - reference to the map instance
-   * @param {Object} maps - reference to the maps library
-   */
-  const onGoogleApiLoaded = ({ map, maps }) => {
-    mapRef.current = map
-    setMapReady(true)
-
-  }
-
-  
-
-  const onMarkerClick = (id, lat, lng ) => {
-    // console.log('This is ->', id)
-    // console.log(lat)
-    // console.log(lng)
-    navigate(`/apartment/${id}`);
-    
-
-
-    // inside the map instance you can call any google maps method
-    mapRef.current.setCenter({ lat, lng })
-    // rif. https://developers.google.com/maps/documentation/javascript/reference?hl=it
-  }
-
-  return (
-    <div style={{width:'100%',height:'100vh',alignContent:'center', boxSizing:'border-box'}}>
-      {mapReady}
-      <GoogleMap
-        apiKey={APIKey}
-        defaultCenter={{ lat: 34.0689, lng: -118.4452 }}
-        defaultZoom={15}
-        mapMinHeight="100vh"
-        onGoogleApiLoaded={onGoogleApiLoaded}
-        onChange={(map) => console.log('Map moved', map)}
-        style={{width:'100%',minHeight:'100vh', boxSizing:'border-box'}}
-      >
-        {markers.map(({ lat, lng, text, id }, index) => (
-          <Marker
-            key={index}
-            lat= {lat}
-            lng={lng}
-            text={text}
-            onClick={() => onMarkerClick( id, lat, lng )} // you need to manage this prop on your Marker component!
-            // draggable={true}
-            // onDragStart={(e, { latLng }) => {}}
-            // onDrag={(e, { latLng }) => {}}
-            // onDragEnd={(e, { latLng }) => {}}
-          /> 
-        ))}   
-      </GoogleMap>
+const Marker = ({ text, onClick }) => (
+    <div className="pin-container" onClick={onClick}>
+        <img src={MapMarker} alt="Pin" />
+        {/* <div className="pin" style={{zIndex:'9999'}}/> */}
+        <p style={{ textAlign: "center", width: "5rem" }}>{text}</p>
     </div>
-  )
-}
+);
 
-export default App
+const App = ({ markers }) => {
+    const navigate = useNavigate();
+
+    if (!APIKey) {
+        console.error(
+            "Did you set the required environment variables in .env?"
+        );
+        process.exit(1); // Exit the application with an error code
+    }
+    const mapRef = useRef(null);
+    const [mapReady, setMapReady] = useState(false);
+
+    /**
+     * @description This function is called when the map is ready
+     * @param {Object} map - reference to the map instance
+     * @param {Object} maps - reference to the maps library
+     */
+    const onGoogleApiLoaded = ({ map, maps }) => {
+        mapRef.current = map;
+        setMapReady(true);
+    };
+
+    const onMarkerClick = (id, lat, lng) => {
+        // console.log('This is ->', id)
+        // console.log(lat)
+        // console.log(lng)
+        navigate(`/apartment/${id}`);
+
+        // inside the map instance you can call any google maps method
+        mapRef.current.setCenter({ lat, lng });
+        // rif. https://developers.google.com/maps/documentation/javascript/reference?hl=it
+    };
+
+    return (
+        <div
+            style={{
+                width: "100%",
+                height: "100vh",
+                alignContent: "center",
+                boxSizing: "border-box",
+            }}
+        >
+            {mapReady}
+            <GoogleMap
+                apiKey={APIKey}
+                defaultCenter={{ lat: 34.0689, lng: -118.4452 }}
+                defaultZoom={15}
+                mapMinHeight="100vh"
+                onGoogleApiLoaded={onGoogleApiLoaded}
+                onChange={(map) => console.log("Map moved", map)}
+                style={{
+                    width: "100%",
+                    minHeight: "100vh",
+                    boxSizing: "border-box",
+                }}
+            >
+                {markers.map(({ lat, lng, text, id }, index) => (
+                    <Marker
+                        key={index}
+                        lat={lat}
+                        lng={lng}
+                        text={text}
+                        onClick={() => onMarkerClick(id, lat, lng)} // you need to manage this prop on your Marker component!
+                        // draggable={true}
+                        // onDragStart={(e, { latLng }) => {}}
+                        // onDrag={(e, { latLng }) => {}}
+                        // onDragEnd={(e, { latLng }) => {}}
+                    />
+                ))}
+            </GoogleMap>
+        </div>
+    );
+};
+
+export default App;
